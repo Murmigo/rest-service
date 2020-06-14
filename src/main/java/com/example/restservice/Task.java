294abf9c064adfb54aa36c4511c397e39e0b70cb
Murmigo <luisguiabarca@gmail.com>
2020-06-14 17:27:32 +0200

    Initial commit

---
+++ src/main/java/com/example/restservice/Task.java
@@ -0,0 +1,54 @@
+package com.example.restservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Integer id;
+	private String description;	
+	private boolean completed;	
+	private String priority;
+	
+	
+	/*public Task(Integer id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.priority = "medium";
+		this.completed = false;
+	}*/
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/restservice/TaskController.java
@@ -0,0 +1,75 @@
+package com.example.restservice;
+
+import java.util.Optional;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+	
+	@Autowired
+	private TaskRepository taskRepo;
+	
+	@GetMapping("/task/{id}")
+	public Optional<Task> task(@PathVariable(value = "id") Integer taskId) {
+
+		return taskRepo.findById(taskId);
+
+	}
+
+	@PostMapping("/task")
+	public String createTask(@RequestBody Task t){
+		Task myTask = new Task();
+		
+		myTask.setDescription(t.getDescription());
+		myTask.setPriority(t.getPriority());
+		myTask.setCompleted(false);
+		taskRepo.save(myTask);
+		
+		return "Saved";
+		
+	}
+	
+	@PutMapping("/task")
+	public String updateTask(@RequestBody Task t){	
+		
+		Task myTask = taskRepo.findById(t.getId()).get();
+		if(myTask != null)
+		{
+			myTask.setDescription(t.getDescription());
+			myTask.setPriority(t.getPriority());
+			myTask.setCompleted(t.isCompleted());
+			taskRepo.save(myTask);
+			return "Update";
+			
+		}else {
+		
+		return "Unable to Updated, id not found";
+		}
+	}
+	
+	@DeleteMapping("/task/{id}")
+	public String deleteTask(@PathVariable(value = "id") Integer taskId) {
+		taskRepo.deleteById(taskId);
+		return "Deleted";
+
+	}
+	
+	@GetMapping("/task")
+	public Iterable<Task> listAllTasks()
+	{
+		return taskRepo.findAll();
+		
+	}
+}
